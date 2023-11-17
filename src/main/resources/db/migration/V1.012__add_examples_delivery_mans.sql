29953e5838230883da6bc292083c3434a2e7e6a7
Filip Jędrzejek <veczarovsky@gmail.com>
2023-11-17 14:14:24 +0100

    Add examples for delivery man

---
+++ src/main/resources/db/migration/V1.012__add_examples_delivery_mans.sql
@@ -0,0 +1,6 @@
+INSERT INTO p_delivery_man (working_area, rating, user_id)
+VALUES
+     ('University Campus', 9, 11),
+      ('Sports Complex', 10, 12),
+      ('Medical District', 3, 13),
+      ('Tourist Hub', 4, 14);
