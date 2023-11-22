91e49e697304e5b65e7d38c14b1766e46dc0ebfe
Kuba Drałus <kubadralus@gmail.com>
2023-11-22 13:14:45 +0100

    small restructuring and login page added

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './modules/auth/login-form/login-form.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: FrontPageComponent, pathMatch: 'full'},
+  {path: 'auth', component: LoginFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { LoginFormComponent } from './modules/auth/login-form/login-form.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    
+    UserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
