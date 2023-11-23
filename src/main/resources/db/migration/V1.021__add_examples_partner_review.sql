2d506e2de1acede99e00ef240f7331eb07a26d00
Filip Jędrzejek <veczarovsky@gmail.com>
2023-11-23 19:09:31 +0100

    PartnerReview migration and example

---
+++ src/main/resources/db/migration/V1.021__add_examples_partner_review.sql
@@ -0,0 +1,7 @@
+INSERT INTO p_partner_review(grade_in_stars,description,date,reviewer_id,rated_id)
+VALUES
+  (4, 'Doskonałe dania, obsługa na najwyższym poziomie.', '2023-01-06', 7, 1),
+  (3, 'Nieźle, ale obsługa mogłaby być lepsza.', '2023-01-07', 8, 1),
+  (2, 'Małe, twarde i kwaśne mirabelki', '2023-01-08', 2, 1),
+  (4, 'Smaczne dania, trochę długi czas oczekiwania.', '2023-01-09', 10, 2),
+  (5, 'Najlepsze jedzenie w mieście, polecam każdemu!', '2023-01-10', 9, 3);
