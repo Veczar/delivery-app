f685aae80c39a26ba8eccbe65dcaeee772381c26
Kuba Drałus <kubadralus@gmail.com>
2024-01-05 13:46:47 +0100

    update api routes permissions

--- src/main/java/org/company/shared/config/SecurityConfiguration.java
+++ src/main/java/org/company/shared/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.company.shared.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,17 +28,30 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-                                .anyRequest().permitAll()
-//                                .requestMatchers("api/auth/**").permitAll()
-//                                .requestMatchers("api/users/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/partners/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/delivery_mans/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/addresses/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/products/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/categories/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/partners/reviews/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/orders/**").hasAuthority("ADMIN")
-//                                .requestMatchers("api/product_order/**").hasAuthority("ADMIN")
+//                                .anyRequest().permitAll()
+                                .requestMatchers("api/auth/**").permitAll()
+                        
+                                .requestMatchers("api/users/**").hasAnyAuthority("ADMIN", "USER") //user only for himself - will check elsewhere
+                                
+                                .requestMatchers("api/partners/**").hasAnyAuthority("ADMIN", "PARTNER")
+                                .requestMatchers(HttpMethod.GET, "/api/partners/**").hasAuthority("USER")
+                                
+                                .requestMatchers("api/delivery_mans/**").hasAuthority("ADMIN")
+                        
+                                .requestMatchers("api/addresses/**").hasAuthority("ADMIN")
+                        
+                                .requestMatchers("api/products/**").hasAnyAuthority("ADMIN", "PARTNER")
+                                .requestMatchers(HttpMethod.GET, "/api/partners/**").hasAuthority("USER")
+                                
+                                .requestMatchers("api/categories/**").hasAnyAuthority("ADMIN", "PARTNER")
+                        
+                                .requestMatchers("api/partners/reviews/**").hasAnyAuthority("ADMIN", "USER", "PARTNER", "COURIER")
+                        
+                                .requestMatchers("api/orders/**").hasAnyAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/partners/**").hasAnyAuthority("USER", "PARTNER")
+                                .requestMatchers(HttpMethod.POST, "/api/partners/**").hasAuthority("USER")
+                        
+                                .requestMatchers("api/product_order/**").hasAnyAuthority("ADMIN", "USER", "PARTNER")
                 )
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
